388f71646e349b6c1e2d07d7453b5c63dc2904e6
Caitlin Cox <caitlincox@g.ucla.edu>
2021-01-03 16:18:13 -0800

    Added joke class so that a joke and the time is printed every 10 seconds. This will later be used to post a joke and the temperature weekly.

--- freezerbot.py
+++ freezerbot.py
@@ -16,12 +16,86 @@ ACCESS_TOKEN = "XXX"        # change XXX to your Twitter Access Token
 ACCESS_SECRET = "XXX"       # change XXX to your Twitter Access Key
 #######
 
-jokes = [
-    ("A duck walks up to a checkout counter to purchase a condom.  The checkout "
-    "clerk asks, \"How would you like to pay for that?\"  The duck says, \"Just "
-    "put it on my bill.\""),
-    "Please write a new joke!"
-]
+class Joke:
+
+    def __init__(self):
+        self.tweeted_joke = False
+        self.count = 0;
+        self.jokes = [
+        ("What has two eyes, six ears, and multiple spleens?"
+        "\n\nThis freezer." 
+        "\n\nIn case you were wondering, there’s a bag of coyote ears on shelf 3."),
+        
+        ("Why did the chicken get into the freezer?"
+        "\n\nTo get to the other side."),
+
+        ("What’s the difference between a dog and a marine biologist?"
+        "\n\nOne wags a tail and the other tags a whale."),
+
+        ("Biology is the only science where multiplication is the same thing as division."),
+
+        ("Why did the bear dissolve in water?"
+        "\n\nIt was polar."),
+
+        ("Why was the function so bent out of shape?"
+        "\n\nIts regression model was too tight a fit."),
+
+        ("How does a physicist deal with a long to-do list?"
+        "\n\nBy truncating it after the first three terms."),
+
+        ("Why do biologists use the abbreviation MAT?"
+        "\n\nSaying the whole thing is a micro-annunciation test."
+        "\n\n (I'm well aware that this is the dumbest one yet, but YOU have the power to fix this."
+        "Send me science jokes. It's never too late.)"),
+
+        ("Stop saying \"rabies-vaccinated,\" and start saying \"raccoon-immune.\""),
+
+        ("Why did the mathematician name his dog Cauchy?"
+        "\n\nBecause he left a residue at every pole."),
+
+        ("Just saw a great film about databases — can’t wait for the SQL."),
+
+        ("A plane was about to take off from Warsaw to England when the captain realized"
+        "there was a problem that would cause the plane to become unstable in the air and crash."
+        "Luckily, he knew how to fix it: he got on the intercom and asked the Poles to move to"
+        "the left half plane."),
+
+        ("An atom and another atom are talking to each other. One atom says, \"I lost an electron.\""
+        "The other replies, \"No way, are you positive?\""),
+
+        ("This was going to be a joke about sodium… but Na."),
+
+        ("Feeling more negative than usual? Disconnected from others? Like you’re just not grounded anymore?"
+        "\n\nYou’re not alone. Every year, thousands of students struggle with charging by induction.")
+    ]
+
+    def tweetJoke(self):
+        joke = self.jokes[self.count]
+        temperature = board.t_in_read(channel)
+        temp_val = "%.2f" % temperature
+        sendTweet(joke + "  Curreent temp is " + temp_val)
+        print(joke)
+
+    def printJokeMondayAtNoon(self):
+        today = datetime.datetime.today()
+        weekday = today.weekday()
+        hour = today.time().hour
+        #if weekday == 0 and hour == 12:
+        if not self.tweeted_joke:
+            # Tweet a joke at noon on Monday.
+            if not self.tweeted_joke:
+                self.tweetJoke()
+                self.tweeted_joke = True
+                if self.count < len(self.jokes):
+                    self.count += 1
+                else:
+                    self.count = 0
+
+        else:
+            self.tweeted_joke = False
+
+
+
 
 channel = 0
 tc_type = TcTypes.TYPE_T
@@ -58,28 +132,6 @@ def findMacAddress():
             log("Failed to connect to internet... will try again soon.")
             time.sleep(5.0)
 
-def tweetRandomJoke():
-    joke = jokes[randrange(len(jokes))]
-    temperature = board.t_in_read(channel)
-    temp_val = "%.2f" % temperature
-    sendTweet(joke + "  Curreent temp is " + temp_val)
-
-tweeted_joke = False
-
-def printJokeMondayAtNoon():
-    """Print a random joke each Monday at noon."""
-    global tweeted_joke
-    today = datetime.datetime.today()
-    weekday = today.weekday()
-    hour = today.time().hour
-    if weekday == 0 and hour == 12:
-        # Tweet a random joke at noon on Monday.
-        if not tweeted_joke:
-            tweetRandomJoke()
-            tweeted_joke = True
-    else:
-        tweeted_joke = False
-
 # Tell the world that we've booted and how to connect over ssh.  To login, use:
 # ssh pi@xxx.xxx.xxx, where xxx.xxx.xxx is the MAC address from the tweet.
 sendTweet("Booted with MAC address " + findMacAddress())
@@ -97,8 +149,11 @@ board = mcc134(hats[0].address)
 # Configure the thermocouple type on the channel 0
 board.tc_type_write(channel, tc_type)
 
+# Create new joke instance
+weeklyJoke = Joke()
+
 while True:
-    printJokeMondayAtNoon()
+    weeklyJoke.printJokeMondayAtNoon()
     error = True
     temperature = board.t_in_read(channel)
     if temperature == mcc134.OPEN_TC_VALUE:
